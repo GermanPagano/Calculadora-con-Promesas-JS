e27dbad7b4338fb2b7c87eb0c4d031049533d7cf
germanpagano <sergiogermanpagano@gmail.com>
2024-06-19 21:50:07 -0300

    update primera funcion

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
+function suma(a, b) {
+    return new Promise((resolve, reject) => {
+        if (a === 0 || b === 0) {
+            reject("operacion innecesaria");
+        } else if ( a + b < 0){
+            reject("solo se aceptan numeros positivos");
+        }
+        else {
+            resolve(a+b);
+        }
+    });
+}
 
-console.log('je');
+
+// Ejemplo de uso:
+suma(20, 5)
+    .then((message) => {
+        console.log(message);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
